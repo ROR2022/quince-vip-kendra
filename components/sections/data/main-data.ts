9dbbc64e53e02d8097eeb492af98b47ca782d19c
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-11-03 09:33:10 -0600

    detalles

--- components/sections/data/main-data.ts
+++ components/sections/data/main-data.ts
@@ -325,7 +325,7 @@ export const quinceMainData = {
   },
   customInvitations:{
     adminPassword: "admin1234",
-    invitationUrl: "https://quince-vip-new-demo.vercel.app/",
+    invitationUrl: "https://quince-vip-kendra.vercel.app/",
     suggested_messages: [
   "¡Querida amiga! Te invito a celebrar conmigo el día más mágico de mi vida. ¡Espero verte brillar junto a mí!",
   "¡Familia querida! Este día especial no sería lo mismo sin ustedes. ¡Los espero con mucho amor!",
